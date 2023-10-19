daee846bc3873fcee9500a30798fd36825aa5f22
Geri Ang <gerillima@gmail.com>
2023-10-19 19:51:09 +0800

    adding and testing for image & doc in telegram message debug 2

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const webhookUrl = process.env.telegramWebhook
 axios.post(`https://api.telegram.org/bot${botToken}/setWebhook`, {
   url: webhookUrl
 })
-.then(response => {
-  console.log('Webhook set successfully:', response.data);
-})
-.catch(error => {
-  console.error('Error setting webhook:', error);
-});
+  .then(response => {
+    console.log('Webhook set successfully:', response.data);
+  })
+  .catch(error => {
+    console.error('Error setting webhook:', error);
+  });
 
 App.listen(process.env.PORT || 5000, () => {  // changed port to 5000
   console.log(`Server is listening on port ${process.env.PORT || 5000}`);
@@ -28,15 +28,23 @@ App.listen(process.env.PORT || 5000, () => {  // changed port to 5000
 App.post('/telegram-webhook', (req, res) => {
   const update = req.body;
   // check if the update contains a message
-  if (update.message) {
+  if (update.message.text) {
     const chatId = update.message.chat.id;
     const text = update.message.text;
-    const image = update.message.photo;
-    const doc = update.message.document.file_id;
     console.log(`Message from chat ${chatId}: ${text}`);
+  }
+  if (update.message.photo) {
+    const chatId = update.message.chat.id;
+    const image = update.message.photo;
     console.log(`Image from chat ${chatId}: ${image}`);
+  }
+  if (update.message.document) {
+    const chatId = update.message.chat.id;
+    const doc = update.message.document.file_id;
     console.log(`Message from chat ${chatId}: ${doc}`);
   }
+
+
   res.sendStatus(200);  // respond to Telegram to acknowledge receipt
 });
 
