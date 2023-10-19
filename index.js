f8c44e19ef5e4159feae7e52899acab0844c9fd0
Geri Ang <gerillima@gmail.com>
2023-10-19 22:03:22 +0800

    adding and testing for image & doc in telegram message debug 6

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ App.use(bodyParser.json());  // to support JSON-encoded bodies
 // Telegram setup
 const botToken = process.env.telegramBotToken
 const webhookUrl = process.env.telegramWebhook
+const telegramApiUrl = `https://api.telegram.org/bot${botToken}`;
+
 
 axios.post(`https://api.telegram.org/bot${botToken}/setWebhook`, {
   url: webhookUrl
@@ -25,7 +27,7 @@ App.listen(process.env.PORT || 5000, () => {  // changed port to 5000
   console.log(`Server is listening on port ${process.env.PORT || 5000}`);
 });
 
-App.post('/telegram-webhook', (req, res) => {
+App.post('/telegram-webhook', async (req, res) => {
   const update = req.body;
   // check if the update contains a message
   if (update.message.text) {
@@ -35,16 +37,40 @@ App.post('/telegram-webhook', (req, res) => {
   }
   if (update.message.photo) {
     const chatId = update.message.chat.id;
-    const image = update.message.photo;
-    for (i of image){
-      console.log(`Image from chat ${chatId}: ${i.file_id}`);
-    }
+    const image_fileId = update.message.photo.file_id;
+    // for (i of image){
+    //   console.log(`Image from chat ${chatId}: ${i.file_id}`);
+    // }
+    console.log(`Message from chat ${chatId}: ${image_fileId}`)
+    
+    try {
+      // Get file path from Telegram
+      const filePathResponse = await axios.post(`${telegramApiUrl}/getFile`, { file_id: image_fileId });
+      const filePath = filePathResponse.data.result.file_path;
+
+      // Download the file
+      const fileUrl = `https://api.telegram.org/file/bot${botToken}/${filePath}`;
+      console.log(`image file url: ${fileUrl}`)
+      // const fileResponse = await axios.get(fileUrl, { responseType: 'stream' });
+      // const writer = fs.createWriteStream('<path-to-save-file>');
+      // fileResponse.data.pipe(writer);
+
+      // writer.on('finish', () => {
+      //     console.log('File downloaded successfully');
+      // });
+      // writer.on('error', (error) => {
+      //     console.error('Error downloading file:', error);
+      // });
+  } catch (error) {
+      console.error('Error retrieving or downloading file:', error);
+  }
+
     
   }
   if (update.message.document) {
     const chatId = update.message.chat.id;
-    const doc = update.message.document.file_id;
-    console.log(`Message from chat ${chatId}: ${doc.file_id}`);
+    const doc_fileId = update.message.document.file_id;
+    console.log(`Message from chat ${chatId}: ${doc_fileId}`);
   }
 
 
