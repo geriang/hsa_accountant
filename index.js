5afb273e315a5288593cb31ebe23b6bc8bc829fa
Geri Ang <gerillima@gmail.com>
2023-10-19 23:31:45 +0800

    integrating astica.ai

--- index.js
+++ index.js
@@ -38,39 +38,68 @@ App.post('/telegram-webhook', async (req, res) => {
   if (update.message.photo) {
     const chatId = update.message.chat.id;
     const image_files = update.message.photo
-    const highest_res = image_files[image_files.length]
+    const highest_res = image_files[image_files.length - 1]
     const image_fileId = highest_res.file_id
     console.log(`Message from chat ${chatId}: ${image_fileId}`)
-    
+
     try {
       // Get file path from Telegram
       const filePathResponse = await axios.post(`${telegramApiUrl}/getFile`, { file_id: image_fileId });
       const filePath = filePathResponse.data.result.file_path;
 
-      // Download the file
+      // Retrieve the file url
       const fileUrl = `https://api.telegram.org/file/bot${botToken}/${filePath}`;
       console.log(`image file url: ${fileUrl}`)
-      // const fileResponse = await axios.get(fileUrl, { responseType: 'stream' });
-      // const writer = fs.createWriteStream('<path-to-save-file>');
-      // fileResponse.data.pipe(writer);
 
-      // writer.on('finish', () => {
-      //     console.log('File downloaded successfully');
+      // sending image to AI model
+      const requestData = {
+        tkn: process.env.asticaApi,  // visit https://astica.ai
+        modelVersion: '2.1_full', // 1.0_full, 2.0_full, or 2.1_full
+        input: fileUrl,
+        visionParams: 'gpt_detailed, text_read', // comma separated, defaults to all
+        gpt_prompt: 'You are a book keeping accountant. Retrieve any date, time, vendor or shop name, purchased items and the cost depicted', // only used if visionParams includes "gpt" or "gpt_detailed"
+        prompt_length: 95 // number of words in GPT response
+      };
+
+      try {
+        const response = await axios.post('https://vision.astica.ai/describe', requestData, {
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        })
+
+        console.log(response.data)
+
+      } catch (error) {
+        console.error("error running astica.ai")
+      }
+      // axios({
+      //   method: 'post',
+      //   url: 'https://vision.astica.ai/describe',
+      //   data: requestData,
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      // }).then((response) => {
+      //   console.log(response.data);
+      // }).catch((error) => {
+      //   console.log(error);
       // });
-      // writer.on('error', (error) => {
-      //     console.error('Error downloading file:', error);
-      // });
-  } catch (error) {
+
+
+    } catch (error) {
       console.error('Error retrieving or downloading file:', error);
+    }
+
   }
 
-    
-  }
-  if (update.message.document) {
-    const chatId = update.message.chat.id;
-    const doc_fileId = update.message.document.file_id;
-    console.log(`Message from chat ${chatId}: ${doc_fileId}`);
-  }
+
+
+  // if (update.message.document) {
+  //   const chatId = update.message.chat.id;
+  //   const doc_fileId = update.message.document.file_id;
+  //   console.log(`Message from chat ${chatId}: ${doc_fileId}`);
+  // }
 
 
   res.sendStatus(200);  // respond to Telegram to acknowledge receipt
