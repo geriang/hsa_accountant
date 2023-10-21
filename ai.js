0a49193490d6ba9cba8f0726638884e27d60c0b1
Geri Ang <gerillima@gmail.com>
2023-10-21 14:10:32 +0800

    testing adding rows to googlesheet 2

--- ai.js
+++ ai.js
@@ -8,13 +8,7 @@ async function processImage(fileUrl){
         modelVersion: '2.1_full', // 1.0_full, 2.0_full, or 2.1_full
         input: fileUrl,
         visionParams: 'gpt_detailed, text_read', // comma separated, defaults to all
-        gpt_prompt: `You are a book keeping accountant. Base on the given information, retrieve the following:
-        1. Date of the Transaction
-        2. Item or Service Description 
-        3. Cost of the Item per quantity
-        4. Quantity of the Item 
-        5. Name of the Vendor
-        and return the information in the same ordered list format. If any of the specified information above cannot be found, just return with a "N.A" `,
+        gpt_prompt: `As a bookkeeping accountant, retrieve these: 1. Transaction Date 2. Item/Service Description 3. Total Cost paid 4. Vendor Name`,
         prompt_length: 95 // number of words in GPT response
       };
     
