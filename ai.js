6e2dbb2f667f8fe526dec7f822df2301562648f2
Geri Ang <gerillima@gmail.com>
2023-10-21 22:46:16 +0800

    main debug 9

--- ai.js
+++ ai.js
@@ -1,14 +1,14 @@
 // sending image to AI model
 const axios = require('axios');
 
-// const asticaApi = process.env.asticaApi
+const asticaApi = process.env.asticaApi
 
-async function processImage(fileUrl) {
+async function processImage(file) {
 
   const requestData = {
-    tkn: process.env.asticaApi,  // visit https://astica.ai
+    tkn: asticaApi,  // visit https://astica.ai
     modelVersion: '2.1_full', // 1.0_full, 2.0_full, or 2.1_full
-    input: fileUrl,
+    input: file,
     visionParams: 'gpt_detailed, text_read', // comma separated, defaults to all
     gpt_prompt: `As a bookkeeping accountant, retrieve these: 1. Transaction Date 2. Item/Service Description 3. Total Cost paid 4. Vendor Name`,
     prompt_length: 95 // number of words in GPT response
