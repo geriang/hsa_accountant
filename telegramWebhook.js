8a36544d92a8da17d87600e7b63f449f032bae70
Geri Ang <gerillima@gmail.com>
2023-10-21 12:58:41 +0800

    refactoring code

---
+++ telegramWebhook.js
@@ -0,0 +1,27 @@
+
+async function setWebhookUrl(botToken, webhookUrl) {
+
+    try {
+        const response = axios.post(`https://api.telegram.org/bot${botToken}/setWebhook`, {
+            url: webhookUrl
+        })
+
+        console.log("Webhook set successfully:", response.data)
+
+    }catch(error){
+
+        console.error("Error setting webhook:", error)
+
+    }
+    
+    //     .then(response => {
+    //     console.log('Webhook set successfully:', response.data);
+    // })
+    //     .catch(error => {
+    //         console.error('Error setting webhook:', error);
+    //     });
+
+}
+
+module.exports = { setWebhookUrl }
+
