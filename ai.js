e98e3154310667a140359355cc6fbd4f422c3a8e
Geri Ang <gerillima@gmail.com>
2023-10-21 14:40:42 +0800

    testing adding rows to googlesheet 4

--- ai.js
+++ ai.js
@@ -3,32 +3,33 @@ const axios = require('axios')
 
 // const asticaApi = process.env.asticaApi
 
-async function processImage(fileUrl){
+async function processImage(fileUrl) {
 
-    const requestData = {
-        tkn: process.env.asticaApi,  // visit https://astica.ai
-        modelVersion: '2.1_full', // 1.0_full, 2.0_full, or 2.1_full
-        input: fileUrl,
-        visionParams: 'gpt_detailed, text_read', // comma separated, defaults to all
-        gpt_prompt: `As a bookkeeping accountant, retrieve these: 1. Transaction Date 2. Item/Service Description 3. Total Cost paid 4. Vendor Name`,
-        prompt_length: 95 // number of words in GPT response
-      };
-    
-      try {
-        const response = await axios.post('https://vision.astica.ai/describe', requestData, {
-          headers: {
-            'Content-Type': 'application/json',
-          }
-        })
-    
-        console.log(response.data)
-        let data = response.data.caption_GPTS
-        return data
-    
-      } catch (error) {
-        console.error("error running astica.ai")
+  const requestData = {
+    tkn: process.env.asticaApi,  // visit https://astica.ai
+    modelVersion: '2.1_full', // 1.0_full, 2.0_full, or 2.1_full
+    input: fileUrl,
+    visionParams: 'gpt_detailed, text_read', // comma separated, defaults to all
+    gpt_prompt: `As a bookkeeping accountant, retrieve these: 1. Transaction Date 2. Item/Service Description 3. Total Cost paid 4. Vendor Name`,
+    prompt_length: 95 // number of words in GPT response
+  };
+
+  try {
+    console.log("Start")
+    const response = await axios.post('https://vision.astica.ai/describe', requestData, {
+      headers: {
+        'Content-Type': 'application/json',
       }
+    })
+
+    console.log(response.data)
+    let data = response.data.caption_GPTS
+    return data
+
+  } catch (error) {
+    console.error("error running astica.ai")
+  }
 
 }
 
-module.exports = {processImage}
+module.exports = { processImage }
