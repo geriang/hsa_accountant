dbd386e1e5beba261811e8cdb413f9a83f10c1b9
Geri Ang <gerillima@gmail.com>
2023-10-21 13:22:42 +0800

    refactoring code 3

---
+++ ai.js
@@ -0,0 +1,30 @@
+// sending image to AI model
+
+async function processImage(fileUrl){
+
+    const requestData = {
+        tkn: process.env.asticaApi,  // visit https://astica.ai
+        modelVersion: '2.1_full', // 1.0_full, 2.0_full, or 2.1_full
+        input: fileUrl,
+        visionParams: 'gpt_detailed, text_read', // comma separated, defaults to all
+        gpt_prompt: 'You are a book keeping accountant. Retrieve any date, time, vendor or shop name, purchased items and the cost depicted', // only used if visionParams includes "gpt" or "gpt_detailed"
+        prompt_length: 95 // number of words in GPT response
+      };
+    
+      try {
+        const response = await axios.post('https://vision.astica.ai/describe', requestData, {
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        })
+    
+        console.log(response.data)
+        return response.data.caption_GPTS
+    
+      } catch (error) {
+        console.error("error running astica.ai")
+      }
+
+}
+
+module.exports = {processImage}
--- telegramWebhook.js
+++ telegramWebhook.js
@@ -1,6 +1,8 @@
 const axios = require('axios')
+const botToken = process.env.telegramBotToken
+const webhookUrl = process.env.telegramWebhook
 
-async function setWebhookUrl(botToken, webhookUrl) {
+async function setWebhookUrl() {
 
     try {
         const response = axios.post(`https://api.telegram.org/bot${botToken}/setWebhook`, {
@@ -14,14 +16,6 @@ async function setWebhookUrl(botToken, webhookUrl) {
         console.error("Error setting webhook:", error)
 
     }
-    
-    //     .then(response => {
-    //     console.log('Webhook set successfully:', response.data);
-    // })
-    //     .catch(error => {
-    //         console.error('Error setting webhook:', error);
-    //     });
-
 }
 
 module.exports = { setWebhookUrl }
