680accc050cc4ad4ed67ad9b247c025f4fcc8c83
Geri Ang <gerillima@gmail.com>
2023-10-22 16:10:04 +0800

    main debug end 7

--- ai.js
+++ ai.js
@@ -10,7 +10,7 @@ async function processImage(file) {
     modelVersion: '2.1_full', // 1.0_full, 2.0_full, or 2.1_full
     input: file,
     visionParams: 'gpt_detailed, text_read', // comma separated, defaults to all
-    gpt_prompt: `As a bookkeeping accountant, indentify and output in this format: 1. Transaction Date 2. Summary of items bought 3. Total Cost 4. Vendor Name`,
+    gpt_prompt: `You are a bookkeeping accountant. Extract: 1. Date: <DDMMYY> 2. Expenses Category: <in 2 words> 3. Total Cost: <number> 4. Vendor: <words>.`,
     prompt_length: 100 // number of words in GPT response
   };
 
