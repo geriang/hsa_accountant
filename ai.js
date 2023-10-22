827494425c6ab4bad09efba92fa9ee0a5c9072e4
Geri Ang <gerillima@gmail.com>
2023-10-22 08:48:16 +0800

    main debug end 6

--- ai.js
+++ ai.js
@@ -10,18 +10,16 @@ async function processImage(file) {
     modelVersion: '2.1_full', // 1.0_full, 2.0_full, or 2.1_full
     input: file,
     visionParams: 'gpt_detailed, text_read', // comma separated, defaults to all
-    gpt_prompt: `As a bookkeeping accountant, indentify and output in this format: 1. Transaction Date 2. Summarised Item/Service Description 3. Total Cost paid 4. Vendor Name`,
-    prompt_length: 95 // number of words in GPT response
+    gpt_prompt: `As a bookkeeping accountant, indentify and output in this format: 1. Transaction Date 2. Summary of items bought 3. Total Cost 4. Vendor Name`,
+    prompt_length: 100 // number of words in GPT response
   };
 
   try {
-    console.log("Start")
     let response = await axios.post('https://vision.astica.ai/describe', requestData, {
       headers: {
         'Content-Type': 'application/json',
       }
     })
-    console.log("start 2")
 
     console.log(response.data)
     let data = response.data.caption_GPTS
